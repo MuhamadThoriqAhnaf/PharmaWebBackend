b34c29f7fb029c91d2baaa966104aaf2395c7c7f
Ananta Pridya <ananta20pridya@gmail.com>
2022-12-08 20:06:27 +0700

    fix: cors

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const cors = require('cors')
+// const cors = require('cors')
 const cookieSession = require("cookie-session");
 const dotenv = require('dotenv');
 dotenv.config();
@@ -14,7 +14,7 @@ var corsOptions = {
 
 // middlewares
 
-app.use(cors());
+// app.use(cors());
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
